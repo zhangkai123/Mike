f59c8ac1572a59423a31cf8caf052adf9134890f
zhangkai123 <176776005@qq.com>
2014-12-30 17:44:00 +0800

    add time in add view controller

--- Mike/Mike/MKAddViewController.m
+++ Mike/Mike/MKAddViewController.m
@@ -9,8 +9,10 @@
 #import "MKAddViewController.h"
 #import "MKCommon.h"
 
-@interface MKAddViewController ()
-
+@interface MKAddViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *theTableView;
+}
 @end
 
 @implementation MKAddViewController
@@ -20,13 +22,107 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancel)];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"保存" style:UIBarButtonItemStylePlain target:self action:@selector(cancel)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"保存" style:UIBarButtonItemStylePlain target:self action:@selector(save)];
     self.navigationItem.leftBarButtonItem.tintColor = UIColorFromRGB(0xd57d9c);
     self.navigationItem.rightBarButtonItem.tintColor = UIColorFromRGB(0xd57d9c);
     self.title = @"记录";
     self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:UIColorFromRGB(0xd57d9c)};
+    
+    theTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 320, self.view.frame.size.height) style:UITableViewStyleGrouped];
+    theTableView.showsHorizontalScrollIndicator = NO;
+    theTableView.showsVerticalScrollIndicator = NO;
+    theTableView.dataSource = self;
+    theTableView.delegate = self;
+//    theTableView.rowHeight = 45;
+//    theTableView.scrollEnabled = NO;
+    [self.view addSubview:theTableView];
+    [theTableView setBackgroundColor:UIColorFromRGB(0xfff1f6)];
+    
+    [theTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+}
+-(void)cancel
+{
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+-(void)save
+{
+    
 }
 
+#pragma uitableview delegate and datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    int sectionRow = 0;
+    if (section == 0) {
+        sectionRow = 1;
+    }else if(section == 1){
+        sectionRow = 2;
+    }else{
+        sectionRow = 1;
+    }
+    return sectionRow;
+}
+//- (CGFloat)tableView:(UITableView*)tableView heightForHeaderInSection:(NSInteger)section
+//{
+//    if (section == 0) {
+//        return 1.0;
+//    }
+//    
+//    return 1.0;
+//}
+
+- (CGFloat)tableView:(UITableView*)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 20.0;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    float rowHeight = 0;
+    if ((indexPath.section == 1)&&(indexPath.row == 0)) {
+        rowHeight = 45;
+    }else{
+        rowHeight = 44;
+    }
+    return rowHeight;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    if (indexPath.row == 0) {
+        UIView *topLineView = [[UIView alloc]initWithFrame:CGRectMake(0, -1, 320, 1)];
+        topLineView.backgroundColor = UIColorFromRGB(0xefdbe2);
+        [cell.contentView addSubview:topLineView];
+    }
+    UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, cell.frame.size.height, 320, 1)];
+    lineView.backgroundColor = UIColorFromRGB(0xefdbe2);
+    [cell.contentView addSubview:lineView];
+    if (indexPath.section == 0) {
+        UILabel *timeStaticLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 5, 60, 34)];
+        timeStaticLabel.text = @"时间";
+        [timeStaticLabel setTextColor:UIColorFromRGB(0xd57d9c)];
+//        timeStaticLabel.backgroundColor = [UIColor blueColor];
+        [cell.contentView addSubview:timeStaticLabel];
+        
+        UILabel *timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(80, 5, 230, 34)];
+        timeLabel.text = @"今天  下午3:20";
+        timeLabel.textAlignment = NSTextAlignmentRight;
+        [timeLabel setTextColor:UIColorFromRGB(0xd57d9c)];
+//        timeLabel.backgroundColor = [UIColor blueColor];
+        [cell.contentView addSubview:timeLabel];
+    }
+    return cell;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
