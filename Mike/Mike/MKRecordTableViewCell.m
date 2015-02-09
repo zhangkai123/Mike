537a64c52765e31808d717cc6d705f7d4d7654a6
zhangkai123 <176776005@qq.com>
2015-02-09 11:03:25 +0800

    note text fit in records part

--- Mike/Mike/MKRecordTableViewCell.m
+++ Mike/Mike/MKRecordTableViewCell.m
@@ -16,35 +16,34 @@
 {
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         
-        self.timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 1, 70, 43)];
-        self.timeLabel.text = @"11 : 49";
-        self.timeLabel.textAlignment = NSTextAlignmentCenter;
-        [self.timeLabel setTextColor:UIColorFromRGB(0xd57d9c)];
-        [self.timeLabel setFont:[UIFont systemFontOfSize:15]];
-        [self addSubview:self.timeLabel];
-
-        self.numberLabel = [[UILabel alloc]initWithFrame:CGRectMake(ScreenWidth/2 - 30, 1, 60, 43)];
-//        self.numberLabel.text = @"200ml";
+        self.numberLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 1, 70, 43)];
         self.numberLabel.textAlignment = NSTextAlignmentCenter;
         [self.numberLabel setTextColor:UIColorFromRGB(0xd57d9c)];
         [self.numberLabel setFont:[UIFont systemFontOfSize:15]];
         [self addSubview:self.numberLabel];
 //        self.numberLabel.backgroundColor = [UIColor yellowColor];
         
-        self.noteLabel = [[UILabel alloc]initWithFrame:CGRectMake(ScreenWidth - 110, 1, 90, 43)];
-        self.noteLabel.text = @"产量爆发呀！";
-        self.noteLabel.textAlignment = NSTextAlignmentCenter;
+        self.noteLabel = [[UILabel alloc]initWithFrame:CGRectMake(80, 1, ScreenWidth - 160, 43)];
+        self.noteLabel.textAlignment = NSTextAlignmentLeft;
         [self.noteLabel setTextColor:UIColorFromRGB(0xd57d9c)];
         [self.noteLabel setFont:[UIFont boldSystemFontOfSize:10]];
+        self.noteLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        self.noteLabel.numberOfLines = 0;
         [self addSubview:self.noteLabel];
-//        self.noteLabel.backgroundColor = [UIColor blueColor];
+//        self.noteLabel.backgroundColor = [UIColor yellowColor];
+        
+        self.timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(ScreenWidth - 70, 1, 50, 43)];
+        self.timeLabel.textAlignment = NSTextAlignmentCenter;
+        [self.timeLabel setTextColor:UIColorFromRGB(0xd57d9c)];
+        [self.timeLabel setFont:[UIFont systemFontOfSize:15]];
+        [self addSubview:self.timeLabel];
+//        self.timeLabel.backgroundColor = [UIColor blueColor];
         
         UIImageView *moreImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"more"]];
         moreImageView.frame = CGRectMake(ScreenWidth - moreImageView.frame.size.width - 8, 30/2, 8, 13);
         [self addSubview:moreImageView];
         
         UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, ScreenWidth, 0.5)];
-//        lineView.backgroundColor = UIColorFromRGB(0xefdbe2);
         lineView.backgroundColor = UIColorFromRGB(0xefdbe2);
         [self addSubview:lineView];
     }
